97131256fc6a5aa9250ad7ed392a3cfd7f00a023
Barreto <v.barreto.and@gmail.com>
2024-07-13 19:49:34 -0300

    feat: add business rule to create and find an order

---
+++ src/main/java/com/e3gsix/fiap/tech_challenge_4_order_management/service/OrderService.java
@@ -0,0 +1,33 @@
+package com.e3gsix.fiap.tech_challenge_4_order_management.service;
+
+import com.e3gsix.fiap.tech_challenge_4_order_management.exceptions.NotFoundException;
+import com.e3gsix.fiap.tech_challenge_4_order_management.model.Order;
+import com.e3gsix.fiap.tech_challenge_4_order_management.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    @Transactional
+    public Order create(Order order) {
+        order.validate();
+        return this.orderRepository.save(order);
+    }
+
+    public Order findById(Long id) {
+        validateExistence(id);
+        return this.orderRepository.findById(id).orElseThrow(() -> createNotFoundOrderException(id));
+    }
+
+    private void validateExistence(Long id) {
+        if (!this.orderRepository.existsById(id)) throw createNotFoundOrderException(id);
+    }
+
+    private NotFoundException createNotFoundOrderException(Long id){
+        return new NotFoundException("Order with ID " + id + " not found.");
+    }
+}
