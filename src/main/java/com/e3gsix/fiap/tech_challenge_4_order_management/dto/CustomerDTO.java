754b30fb65c1013f83484cbce77ab96b49f678f1
Barreto <v.barreto.and@gmail.com>
2024-07-15 19:50:30 -0300

    feat: add customer and products feing clients

---
+++ src/main/java/com/e3gsix/fiap/tech_challenge_4_order_management/dto/CustomerDTO.java
@@ -0,0 +1,8 @@
+package com.e3gsix.fiap.tech_challenge_4_order_management.dto;
+
+public class CustomerDTO {
+    private Long id;
+    private String name;
+    private String uf;
+    private String address;
+}
